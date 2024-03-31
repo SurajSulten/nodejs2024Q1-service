08050ee89c9ed83f5fd5264cb9543f537ab08b73
Suroj <suraj.sulten@gmail.com>
2024-03-31 11:13:11 +0500

    create artist

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { AlbumController } from './album/album.controller';
 import { AlbumModule } from './album/album.module';
+import { ArtistController } from './artist/artist.controller';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
-  imports: [UserModule, AlbumModule],
-  controllers: [AppController, UserController, AlbumController],
+  imports: [UserModule, AlbumModule, ArtistModule],
+  controllers: [AppController, UserController, AlbumController, ArtistController],
   providers: [AppService],
 })
 export class AppModule {}
