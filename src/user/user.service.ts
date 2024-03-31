f9f6dcdb95d014ddebec4a3213aa4485a12e8be7
Suroj <suraj.sulten@gmail.com>
2024-03-31 09:48:13 +0500

    feat: user updatePassword

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { BadRequestException, ForbiddenException, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import{ v4 as uuidv4} from 'uuid';
 import { CreateUserDto } from './dto/create-user.dto';
+import { validateIdFormat } from 'src/helpers/validateIdFormat';
 // import { IUser } from 'src/types/interfaces';
 // import {
 //     deleteEntityFromCollection,
@@ -39,12 +41,13 @@ export class UserService {
                 'Request body does not contain required fields or their format is npt correct' 
             );
         }
+        const currentTimestamp = Date.now();
         const newUser = {
             ...userDto,
             id: uuidv4(),
             version: 1,
-            createdAt: Date.now(),
-            updatedAt: Date.now(),
+            createdAt: currentTimestamp,
+            updatedAt: currentTimestamp,
         };
         this.db.users.push(newUser);
         const { password, ...userWithoutPassword } = newUser;
@@ -53,5 +56,38 @@ export class UserService {
 
     async deleteUser(id: string) {
         deleteEntityFromCollection(id, this.db.users);
+    }
+
+    async updatePassword(UpdateUserDto: UpdateUserDto, id: string) {
+        if(
+            !UpdateUserDto.oldPassword ||
+            !UpdateUserDto.newPassword || 
+            typeof updateUserDto.oldPassword !== 'string' ||
+            typeof updateUserDto.newPassword !== 'string' 
+        ) {
+            throw new BadRequestException(
+                'Request body does not contain required fields or their format is npt correct'
+            );
+        }
+        validateIdFormat(id);
+        const user: IUser = this.db.users.find((user) => user.id === id);
+
+        if(user) {
+            if(updateUserDto.oldPassword !== user.password) {
+                throw new ForbiddenException('Old password is wrong');
+            }
+            const updateUser = {
+                ...user,
+                password: updateUserDto.newPassword,
+                version: user.version + 1,
+                updatedAt: Date.now() 
+            };
+            const userIdx = this.db.users.indexOf(user);
+            this.db.users[userIdx] = updateUser;
+            const { password, ...userWithoutPassword } = updatedUser;
+            return userWithoutPassword;
+        } else {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
     }
 }
