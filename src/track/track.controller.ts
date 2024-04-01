47907b28234ea25dd0814c4ea9495396fbddeeb0
Suroj <suraj.sulten@gmail.com>
2024-04-01 09:03:28 +0500

    do track controller and service

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import { TrackService } from './track.service';
 
 @Controller('track')
-export class TrackController {}
+export class TrackController {
+    constructor(private readonly trackService: TrackService) {}
+
+    @Get()
+    async getAll() {
+        return await this.trackService.getTracks();
+    }
+
+    @Get(':id')
+    async getByID(@Param('id') id: string) {
+        return await this.trackService.getTrackById(id);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    async create(@Body() createTrackDto: CreateTrackDto) {
+        return await this.trackService.createTrack(createTrackDto);
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT) 
+    async delete(@Param('id') id: string) {
+        return await this.trackService.deleteTrack(id);
+    }
+
+    @Put(':id')
+    async update(
+        @Body() updateTrackDto: UpdateTrackDto,
+        @Param('id') id: string,
+    ) {
+        return await this.trackService.updateTrack(updateTrackDto, id);
+    }
+}
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { DB } from 'src/db';
+import { validateIdFormat } from 'src/helpers/validateIdFormat';
 
 @Injectable()
-export class TrackService {}
+export class TrackService {
+    constructor(@Inject('DB_CONNECTION') private readonly db: DB) {}
+
+    private isInvalidDto(dto: CreateTrackDto | UpdateTrackDto) {
+        return !dto.name || !isIdValid(dto.albumId) || !isIdValid(dto.artistId);
+    }
+
+    async getTracks() {
+        return this.db.tracks;
+    }
+
+    async getTrackById(id: string) {
+        return getEntityById<ITrack>(id, this.db.tracks);
+    }
+
+    async createTrack(createTrackDto: CreateTrackDto) {
+        if(this.isInvalidDto(createTrackDto)) {
+            throw new BadRequestException(
+                'Request body does not contain required fields or their format is not correct'
+            )
+        } else {
+            return addEntityToCollection(createTrackDto, this.db.tracks);
+        }
+    }
+
+    async deleteTrack(id: string) {
+        deleteEntityFromCollection(id, this.db.tracks);
+        deleteFromFavs(id, this.db.favs.tracks);
+    }
+
+    async updateTrack(updateTrackDto: UpdateTrackDto, id: string) {
+        if(this.isInvalidDto(updateTrackDto)) {
+            throw new BadRequestException(
+                'Request body does not contain required fields or their format is not correct'
+            );
+        }
+        validateIdFormat(id);
+        const updatedTrack = updateEntityInCollection<ITrack>(
+            id,
+            updateTrackDto,
+            this.db.traks,
+        );
+        return updatedTrack;
+    }
+}
