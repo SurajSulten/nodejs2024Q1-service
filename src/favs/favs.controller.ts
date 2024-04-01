c0dcb3f2fe0736181aa4953ab7ee4a9e4979abf3
Suroj <suraj.sulten@gmail.com>
2024-04-01 09:54:30 +0500

    feat: do favs controller

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import { FavsService } from './favs.service';
 
 @Controller('favs')
-export class FavsController {}
+export class FavsController {
+    constructor(private readonly favsService: FavsService) {}
+
+    @Get()
+    async getAll() {
+        return await this.favsService.getFavs();
+    }
+
+    @Post('track/:id')
+    @HttpCode(HttpStatus.CREATED)
+    async addFavTrack(@Param('id') id: string) {
+        return await this.favsService.addTrack(id);
+    }
+
+    @Post('album/:id')
+    @HttpCode(HttpStatus.CREATED)
+    async addFavAlbum(@Param('id') id: string) {
+        return await this.favsService.addAlbum(id);
+    }
+
+    @Post('artist/:id')
+    @HttpCode(HttpStatus.CREATED)
+    async addFavArtist(@Param('id') id: string) {
+        return await this.favsService.addArtist(id);
+    }
+
+    @Delete('track/:id')
+    @HttpCode(HttpStatus.CREATED)
+    async deleteFavTrack(@Param('id') id: string) {
+        return await this.favsService.deleteTrack(id);
+    }
+
+    @Delete('album/:id')
+    @HttpCode(HttpStatus.CREATED)
+    async deleteFavAlbum(@Param('id') id: string) {
+        return await this.favsService.deleteAlbum(id);
+    }
+
+    @Delete('artist/:id')
+    @HttpCode(HttpStatus.CREATED)
+    async deleteFavArtist(@Param('id') id: string) {
+        return await this.favsService.deleteArtist(id);
+    }   
+}
--- src/favs/favs.module.ts
+++ src/favs/favs.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { FavsService } from './favs.service';
+import { FavsController } from './favs.controller';
+import { db } from 'src/db'
 
 @Module({
-  providers: [FavsService]
+  controllers: [FavsController],
+  providers: [
+    FavsService,
+    {
+      provide: 'DB_CONNECTION',
+      useValue: db
+    }
+  ]
 })
 export class FavsModule {}
