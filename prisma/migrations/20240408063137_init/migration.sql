f61f32fc23270850763a6e367fa0940ef8aecba0
Suroj <suraj.sulten@gmail.com>
2024-04-08 11:32:17 +0500

    migrate prisma

---
+++ prisma/migrations/20240408063137_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "version" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
